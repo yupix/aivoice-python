95a04ca0ddd67d7e5f8333ef6d70fd55e7825dbe
yupix <yupi0982@outlook.jp>
2025-07-05 00:20:52 +0900

    fix: バージョン番号を0.1.0から0.1.1に更新

--- aivoice_python/__init__.py
+++ aivoice_python/__init__.py
@@ -6,7 +6,7 @@ A Python library for controlling A.I.VOICE Editor through its API.
 
 from .aivoice_control import AIVoiceTTsControl, HostStatus, TextEditMode
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "yupix"
 __email__ = "yupi0982@outlook.jp"
 
